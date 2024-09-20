17a677e6494b61a2436f2e1acbbf70dc718783e2
Kriti <kriti.srivastava002@gmail.com>
2024-09-20 20:03:46 +0530

    Factory Design Pattern- Drawing Shapes

---
+++ Factory Design Pattern- Shapes/main.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+class Shape {
+public:
+    virtual void draw() = 0;
+};
+
+class Circle : public Shape {
+public:
+    int radius;
+    Circle(int r){
+        radius= r;
+    }
+    virtual void draw() {
+        cout << "Drawing a circle of radius " << radius << "\n";
+    }
+};
+
+class Square : public Shape {
+public:
+    int side;
+    Square(int s){
+        side =s;
+    }
+    virtual void draw() {
+        cout << "Drawing a square of side " << side << "\n";
+    }
+};
+
+class ShapeFactory {
+public:
+    Shape* getShape(char ch) {
+        switch (ch) {
+            case 'C': {
+                int r;
+                cout << "Input the Radius: ";
+                cin >> r;
+                return new Circle(r);
+            }
+            case 'S': {
+                int s;
+                cout << "Input the side of Square: ";
+                cin >> s;
+                return new Square(s);
+            }
+            default:
+                cout << "Invalid shape type!" << endl;
+                return nullptr;
+        }
+    }
+};
+
+
+
+int main() {
+    ShapeFactory *shapeFactory = new ShapeFactory();
+    char ch;
+    cout<<"Enter C for drawing a Circle and S for drawing a square: ";
+    cin>>ch;
+    Shape *s = shapeFactory->getShape(ch);
+    s->draw();
+
+    return 0;
+}
